b1736e33394804305656f12183dc684c0480efdb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-22 17:09:08 +0800

    Delete Counter_Tramelblaze directory

--- Counter_Tramelblaze/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Rosswell/Documents/460/Counter_Tramelblaze/Top_Level_tb_isim_beh.exe" -prj "C:/Users/Rosswell/Documents/460/Counter_Tramelblaze/Top_Level_tb_beh.prj" "work.Top_Level_tb" "work.glbl" 
